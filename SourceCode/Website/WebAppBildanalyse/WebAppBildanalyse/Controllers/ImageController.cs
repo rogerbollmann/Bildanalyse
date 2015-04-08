2d7dcdd34f1e7adedd8d0f5f2568f49d9a78c456
rogerbollmann <roger.bollmann@gmail.com>
2015-04-08 20:38:58 +0200

    Webseite und Sender hinzugefügt

---
+++ SourceCode/Website/WebAppBildanalyse/WebAppBildanalyse/Controllers/ImageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppBildanalyse.Models;
+
+namespace WebAppBildanalyse.Controllers
+{
+    public class ImageController : Controller
+    {
+        //
+        // GET: /Image/
+        public ActionResult Index()
+        {
+            var model = new Images()
+            {
+                Gallery = Directory.EnumerateFiles(Server.MapPath("~/Images"))
+                          .Select(fn => "~/Images/" + Path.GetFileName(fn)).ToList()
+            };
+
+            return View(model);
+        }
+	}
+}
